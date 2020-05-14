40899ba3993a3639b466a6e47a7a81b284164d8e
Bin Wu <binwu@google.com>
2020-05-14 07:07:45 +0000

    minor updates

--- k8s/gke/elastic/bin/uninstall_ECK.sh
+++ k8s/gke/elastic/bin/uninstall_ECK.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# delete all underlying Elastic resources (pods, secrets, services etc.)
 kubectl get namespaces --no-headers -o custom-columns=:metadata.name \
       | xargs -n1 kubectl delete elastic --all -n
 
