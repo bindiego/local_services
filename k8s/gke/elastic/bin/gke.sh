f4d18c1915c263127e92166b4534c0d23b89ba7f
Bin Wu <binwu@google.com>
2020-09-27 01:24:42 +0000

    enable dataplane v2 using eBPF for GKE on GCP

--- k8s/gke/elastic/bin/gke.sh
+++ k8s/gke/elastic/bin/gke.sh
@@ -24,6 +24,7 @@ __create() {
         --region "${region}" \
         --node-locations "${region}-a","${region}-b" \
         --no-enable-basic-auth \
+        --enable-dataplane-v2 \
         --release-channel "rapid" \
         --machine-type "$machine_type" \
         --image-type "COS" \
